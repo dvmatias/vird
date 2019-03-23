f3811180befc4d4e3adb8e897ba53b8990392ebe
matidelvecchio <matias.delv.dom@gmail.com>
2019-03-23 17:48:47 -0300

    First commit!

---
+++ app/src/main/java/com/matias/vird/screens/splash/SplashActivity.java
@@ -0,0 +1,15 @@
+package com.matias.vird.screens.splash;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.matias.vird.R;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
